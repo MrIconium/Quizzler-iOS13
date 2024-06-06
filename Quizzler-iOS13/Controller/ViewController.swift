5c91525d20230e8304ffa86f443005e37a478066
Mehmet Emrah Konya <mehmetemrahk@gmail.com>
2024-06-06 16:02:19 +0300

    Add functionality

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionText: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func answerButtons(_ sender: UIButton) {
+        
+        if let titleText = sender.titleLabel?.text {
+            print(titleText)
+        }
+    }
+    
 }
 
---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Mehmet Emrah Konya on 6.06.2024.
+//  Copyright © 2024 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question{
+    var question: String
+    var answer: String
+}
